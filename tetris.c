6957439e9123b9422002cc1d02e9abe2511c94cf
broccolingual <broccolingual@gmail.com>
2022-04-12 10:10:13 +0900

    add: add tetris.h

--- tetris.c
+++ tetris.c
@@ -7,28 +7,7 @@
 
 #include <ncurses.h>
 
-#define FIELD_WIDTH   10
-#define FIELD_HEIGHT  20
-#define FIELD_HEIGHT_MARGIN 4
-#define BLOCK_MAX 7
-#define INTERVAL 0.5
-
-#define WIDTH_RATIO 2
-#define HEIGHT_RATIO 1
-
-typedef struct {
-  int x;
-  int y;
-} POSITION;
-
-typedef struct {
-  POSITION p[4];
-} BLOCK;
-
-typedef struct {
-  BLOCK type;
-  POSITION p;
-} TARGET;
+#include "tetris.h"
 
 BLOCK BLOCKS[BLOCK_MAX] = {
   // BLOCK_I
@@ -66,27 +45,6 @@ BLOCK BLOCKS[BLOCK_MAX] = {
     {{0, 0}, {0, -1}, {1, 0}, {-1, 0}}
   }
 };
-
-void makeField();
-void drawInst(int cx, int cy);
-void drawScore(int cx, int cy);
-void drawGameover(int cx, int cy);
-void drawField(int cx, int cy);
-void refreshField();
-BLOCK rotateBlock();
-BLOCK selectRandomBlock();
-void setCurrentBlock();
-void updateBlock(int state);
-bool canMove(int dx, int dy);
-bool canRotate();
-bool changeBlockState();
-void searchAlign();
-void deleteAlign(int dy);
-void moveUP();
-void moveDOWN();
-void moveRIGHT();
-void moveLEFT();
-bool checkGameover();
 
 int POINT = 0;
 int FIELD[FIELD_HEIGHT+FIELD_HEIGHT_MARGIN][FIELD_WIDTH];
---
+++ tetris.h
@@ -0,0 +1,43 @@
+#define FIELD_WIDTH   10
+#define FIELD_HEIGHT  20
+#define FIELD_HEIGHT_MARGIN 4
+#define BLOCK_MAX 7
+#define INTERVAL 0.5
+
+#define WIDTH_RATIO 2
+#define HEIGHT_RATIO 1
+
+typedef struct {
+  int x;
+  int y;
+} POSITION;
+
+typedef struct {
+  POSITION p[4];
+} BLOCK;
+
+typedef struct {
+  BLOCK type;
+  POSITION p;
+} TARGET;
+
+void makeField();
+void drawInst(int cx, int cy);
+void drawScore(int cx, int cy);
+void drawGameover(int cx, int cy);
+void drawField(int cx, int cy);
+void refreshField();
+BLOCK rotateBlock();
+BLOCK selectRandomBlock();
+void setCurrentBlock();
+void updateBlock(int state);
+bool canMove(int dx, int dy);
+bool canRotate();
+bool changeBlockState();
+void searchAlign();
+void deleteAlign(int dy);
+void moveUP();
+void moveDOWN();
+void moveRIGHT();
+void moveLEFT();
+bool checkGameover();
